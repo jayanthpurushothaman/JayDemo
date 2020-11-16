417e97b6504f5ae97a68fa58c425802c7aaec9f1
Jayanth P <jayanth.purushothaman@photoninfotech.net>
2020-11-16 23:01:57 +0530

    Initial Commit

---
+++ JayDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JayDemo
+//
+//  Created by Jayanth P on 16/11/20.
+//  Copyright © 2020 Jayanth P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
